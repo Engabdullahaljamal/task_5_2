fec9ea50c47ba9e182c0126bc0c5cdcd188f102a
abdullah <en>
2023-10-14 21:25:10 +0300

    task5_2 edit new arrivals section

--- assets/script.js
+++ assets/script.js
@@ -11,38 +11,41 @@ let i = 0;
 
 function displayData(arr) {
     arr.forEach(function (item) {
+        i++;
+        if (i <= 4) {
+            const cardsArea = document.querySelector('.cards_area');
+            const div1 = document.createElement('div');
+            div1.classList = "card";
 
-        const cardsArea = document.querySelector('.cards_area');
-        const div1 = document.createElement('div');
-        div1.classList = "card";
 
+            const img = document.createElement("img")
+            img.src = item.images[0];
+            img.classList = "card-img-top img_style"
 
-        const img = document.createElement("img")
-        img.src = item.images[0];
-        img.classList = "card-img-top img_style"
+            const div2 = document.createElement("div")
+            div2.classList = "card-body"
 
-        const div2 = document.createElement("div")
-        div2.classList = "card-body"
+            const h5 = document.createElement("h5");
+            h5.classList = "title text-center"
+            h5.innerText = item.title;
 
-        const h5 = document.createElement("h5");
-        h5.classList = "title text-center"
-        h5.innerText = item.title;
+            const p = document.createElement("p");
+            p.classList = "card-text text-center"
+            p.innerText = item.description;
+            let w = "$"
+            const p2 = document.createElement("p");
+            p2.classList = "card-text text-center red_p"
+            p2.innerText = "the price: " + item.price + "$";
 
-        const p = document.createElement("p");
-        p.classList = "card-text text-center"
-        p.innerText = item.description;
-        let w = "$"
-        const p2 = document.createElement("p");
-        p2.classList = "card-text text-center red_p"
-        p2.innerText = "the price: " + item.price + "$";
+            div2.append(h5);
+            div2.append(p);
+            div2.append(p2)
+            div1.append(img);
+            div1.append(div2);
 
-        div2.append(h5);
-        div2.append(p);
-        div2.append(p2)
-        div1.append(img);
-        div1.append(div2);
-
-        cardsArea.append(div1);
+            cardsArea.append(div1);
+        }
+        else;
 
     });
 
@@ -51,8 +54,16 @@ function displayData(arr) {
 const dataFromLocalStorage = localStorage.getItem('data');
 
 
+let selectField = document.getElementById("selectField");
+let selectText = document.getElementById("selectText");
+let options = document.getElementsByClassName("options");
 
+for (option of options) {
+    option.onclick = function () {
+        selectText = "";
+        selectText.innerHTML = this.textContent;
 
-
+    }
+}
 
 
